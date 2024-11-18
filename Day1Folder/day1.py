5fd5149fc206c5e80cdddf5f4189e239bacea984
yihemg <yiheng.tan.yh@gmail.com>
2024-11-18 04:36:12 +0000

    day 1
    Co-authored-by: Computhink-Alfred <Computhink-Alfred@users.noreply.github.com>

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -1,4 +1,33 @@
 # Write all your codes for Day 1 here.
 # REMEMBER to change main.py to import this file.
 # You can COMMENT out the previous task before going on to the next task
-print("hello from day1")
+# print("hello from day1")
+# print("I am Yiheng")
+# print("I am 13 years old")
+# print("I attend Ang Mo Kio Secondary School")
+# print("If i had $2000 i would buy pokemon cards and gems")
+
+# FirstName = "Yiheng"
+# SecondName="Tan"
+# print(FirstName + " " +SecondName)
+
+# num1 = 88888888888888888888888
+# num2 = "88888888888888888888888"
+# print(num1 + int(num2))
+
+
+
+# def introducemyself(name, food):
+#     print("my name is " + food+name)
+#     print("my favourite " +food)
+#     print("give ur money to me")
+#     print("how to sing the sigma song")
+
+introducemyself("rabbits","fast")
+
+def findingnumbers(num1,num2):
+    ans=num1+num2
+    print(ans)
+
+findingnumbers(1,1)
+    
--- Day1Folder/minecraft_day1.py
+++ Day1Folder/minecraft_day1.py
@@ -1,6 +1,37 @@
-# AT THE END OF LESSON, COPY YOUR CODE FROM MINECRAFT HERE.
-# THIS IS SO THAT YOU HAVE A RECORD OF YOUR CODE FROM MINECRAFT CODE BUILDER
-################# Function Section #############################
+def Teleport():
+    agent.teleport_to_player()
 
+player.on_chat("come",Teleport)
 
-################## On Chat Commands Section #####################
+def moveForward():
+     agent.move(FORWARD, 1)
+
+def moveBack():
+    agent.move(BACK, 1)
+
+def moveUp():
+    agent.move(UP,1)
+
+def moveDown():
+    agent.move(DOWN,1)   
+
+def moveLeft():
+    agent.move(LEFT,1)
+
+def moveRight():
+    agent.move(RIGHT,1)
+
+def turnRight():
+    agent.move(RIGHT,1)
+
+def turnLeft():
+    agent.move(LEFT,1)
+
+player.on_chat("fw",moveForward)    
+player.on_chat("bk",moveBack)
+player.on_chat("ml",moveLeft)
+player.on_chat("mr",moveRight)
+player.on_chat("up",moveUp)
+player.on_chat("down",moveDown)
+player.on_chat("tl",turnLeft)
+player.on_chat("tr",turnRight)
