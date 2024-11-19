b97c33ab0220e945b825a9e42f3dccca04fb0aee
yihemg <yiheng.tan.yh@gmail.com>
2024-11-19 04:26:04 +0000

    Day 2
    Co-authored-by: Computhink-Alfred <Computhink-Alfred@users.noreply.github.com>

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -1,4 +1,21 @@
 # Write all your codes for Day 2 here.
 # REMEMBER to change main.py to import this file.
 # You can COMMENT out the previous task before going on to the next task
-print("hello from day2")
+# print("hello from day2")
+# for i in range(10):
+#     print(str(i+1)+".I will not sling mud at my friends if they give me")
+
+# name ="nike"
+# for i in (name):
+#     print("Give me a " + i +"!")
+# print(name+"!")    
+
+# for count in range(1,11):
+#     print(count)
+
+# for count in range(5,33):
+#     print(count)
+
+# for count in range(50,0,-1):
+#     print(count)
+
--- Day2Folder/minecraft_day2.py
+++ Day2Folder/minecraft_day2.py
@@ -1,2 +1,62 @@
-# AT THE END OF LESSON, COPY YOUR CODE FROM MINECRAFT HERE.
-# THIS IS SO THAT YOU HAVE A RECORD OF YOUR CODE FROM MINECRAFT CODE BUILDER
+def Teleport():
+    agent.teleport_to_player()
+
+player.on_chat("come",Teleport)
+
+def moveForward(steps):
+     agent.move(FORWARD, steps)
+
+def moveBack(steps):
+    agent.move(BACK, steps)
+
+def moveUp(steps):
+    agent.move(UP,steps)
+
+def moveDown(steps):
+    agent.move(DOWN,steps)   
+
+def moveLeft(steps):
+    agent.move(LEFT,steps)
+
+def moveRight(steps):
+    agent.move(RIGHT,steps)
+
+def turnRight():
+    agent.turn(RIGHT)
+
+def turnLeft():
+    agent.turn(LEFT)
+
+player.on_chat("fw",moveForward)    
+player.on_chat("bk",moveBack)
+player.on_chat("ml",moveLeft)
+player.on_chat("mr",moveRight)
+player.on_chat("up",moveUp)
+player.on_chat("down",moveDown)
+player.on_chat("tl",turnLeft)
+player.on_chat("tr",turnRight)
+
+
+
+def destroytree(steps):
+    for i in range(steps):
+        agent.destroy(FORWARD)
+        agent.move(UP,1)
+    agent.move(DOWN,steps)
+    agent.collect_all()
+player.on_chat("tree",destroytree)  
+
+
+
+def destroystone(steps):
+    for i in range(steps):
+        agent.destroy(LEFT)
+        agent.destroy(RIGHT)
+        agent.destroy(FORWARD)
+        agent.destroy(DOWN)
+        agent.collect_all()     
+        agent.move(FORWARD,1)
+    
+player.on_chat("df",destroystone)       
+
+
