d48090acc389f575202f7c426128f8b7a3ef4cb5
yihemg <yiheng.tan.yh@gmail.com>
2024-11-21 04:55:22 +0000

    Co-authored-by: Computhink-Alfred <Computhink-Alfred@users.noreply.github.com>

--- Day4Folder/day4.py
+++ Day4Folder/day4.py
@@ -2,3 +2,30 @@
 # REMEMBER to change main.py to import this file.
 # You can COMMENT out the previous task before going on to the next task
 print("hello from day4")
+
+# counter=0
+
+# while counter<5:
+#     print(counter)
+#     counter=counter+1
+
+# counter=5
+
+# while counter<33:
+#     print(counter)
+#     counter=counter+1
+
+# counter=50
+
+# while counter>0:
+#     print(counter)
+#     counter=counter-1
+
+
+userans=input("what is a bean that could talk? u will never guess it:)")
+
+while userans!= "Mr Bean":
+        print("u lost 1000 aura. current aura -1000")
+        userans=input("what is a bean that could talk? u will never guess it:)")
+
+print("u are correct(never)")
--- Day4Folder/minecraft_day4.py
+++ Day4Folder/minecraft_day4.py
@@ -1,2 +1,100 @@
-# AT THE END OF LESSON, COPY YOUR CODE FROM MINECRAFT HERE.
-# THIS IS SO THAT YOU HAVE A RECORD OF YOUR CODE FROM MINECRAFT CODE BUILDER
+def Teleport():
+    agent.teleport_to_player()
+
+player.on_chat("come",Teleport)
+
+def moveForward(steps):
+     agent.move(FORWARD, steps)
+
+def moveBack(steps):
+    agent.move(BACK, steps)
+
+def moveUp(steps):
+    agent.move(UP,steps)
+
+def moveDown(steps):
+    agent.move(DOWN,steps)   
+
+def moveLeft(steps):
+    agent.move(LEFT,steps)
+
+def moveRight(steps):
+    agent.move(RIGHT,steps)
+
+def turnRight():
+    agent.turn(RIGHT)
+
+def turnLeft():
+    agent.turn(LEFT)
+
+player.on_chat("fw",moveForward)    
+player.on_chat("bk",moveBack)
+player.on_chat("ml",moveLeft)
+player.on_chat("mr",moveRight)
+player.on_chat("up",moveUp)
+player.on_chat("down",moveDown)
+player.on_chat("tl",turnLeft)
+player.on_chat("tr",turnRight)
+
+
+
+def destroytree(steps):
+    for i in range(steps):
+        agent.destroy(FORWARD)
+        agent.move(UP,1)
+    agent.move(DOWN,steps)
+    agent.collect_all()
+player.on_chat("tree",destroytree)  
+
+
+
+def destroystone(steps):
+    for i in range(steps):
+        agent.destroy(LEFT)
+        agent.destroy(RIGHT)
+        agent.destroy(FORWARD)
+        agent.destroy(DOWN)
+        agent.collect_all()     
+        agent.move(FORWARD,1)
+    
+player.on_chat("df",destroystone)       
+
+
+def buildwall():
+    for i in range(5):
+        for i in range(3):
+            agent.place(FORWARD)
+            agent.move(UP,1)
+        agent.move(DOWN,3)
+        agent.move(RIGHT,1)
+
+player.on_chat("wall", buildwall)
+
+def solvespiral():
+    while agent.inspect(AgentInspection.BLOCK,DOWN) != DIAMOND_BLOCK:
+        if not agent.detect(AgentDetection.BLOCK,RIGHT):
+            agent.turn(RIGHT)
+
+        agent.move(FORWARD,1)
+
+player.on_chat("findway",solvespiral) 
+
+def solvemaze():
+    while agent.detect(AgentDetection.BLOCK,DOWN) == True:
+        if not agent.detect(AgentDetection.BLOCK, LEFT):
+            agent.turn(LEFT)
+            agent.move(FORWARD,1)
+        elif not agent.detect(AgentDetection.BLOCK,FORWARD):
+            agent.move(FORWARD,1)
+        else:
+            agent.turn(RIGHT)
+            agent.move(FORWARD,1) 
+    
+player.on_chat("maze",solvemaze)
+
+def digunderground():
+    while agent.detect(AgentDetection.BLOCK,DOWN):
+        agent.destroy(DOWN)
+        agent.move(DOWN,1)
+
+player.on_chat("under",digunderground)
